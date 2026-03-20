2b407cf5d26f9f5e0b261006bb364c7ee2e431ca
Tiimie1 <tim.pangersic@gmail.com>
2026-03-20 15:28:49 +0100

    gh pages

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import RWAStyleguide from "../RWAStyleguideQ22026.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RWAStyleguide />
+  </StrictMode>
+);
